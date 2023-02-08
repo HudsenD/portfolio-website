b7166d9304fad284354952ad5e1c7033ee821ab6
Hudsen Durst <hudsendurst@pop-os.localdomain>
2023-02-08 08:19:41 -0600

    about me update

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -6,10 +6,10 @@ const AboutMe = () => {
             <div className="max-w-[1240px] mx-auto px-2 py-16">
                 <p className="mb-2 text-xl font-bold tracking-widest uppercase">About Me</p>
                 <p className="">
-                    Disciplined, detail-oriented, self-taught developer, believe blockchain is imperative for our
-                    future, love to learn, and have almost 2 years of experience participating in web3. Technical
-                    proficiencies include: Javascript, Solidity, Hardhat, EthersJS, NextJS, The Graph, and Moralis.
-                    Always eager to learn more!
+                    As a highly motivated and dedicated developer, I bring nearly two years of experience in the web3
+                    ecosystem and a passion for blockchain technology. My technical proficiencies include Javascript,
+                    Solidity, EVM, Hardhat, EthersJS, NextJS, The Graph, and Moralis, and I am always seeking new
+                    opportunities to expand my knowledge base.
                 </p>
             </div>
         </div>
